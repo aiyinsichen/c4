0ba3a33e609d4c5443f44a1350dc433e6aa9ed98
aiyinsichen <aiyinsichen@qq.com>
2019-01-17 09:19:47 +0800

    添加替换功能

--- main.c
+++ main.c
@@ -1,67 +1,107 @@
 #include <stdio.h>
 char arr[1024];
 int index = 0;
+int isMingan(char x)
+{
+    int count = 0;
+    for (int i = 0; i < index; i++)
+    {
+        if (x == arr[i])
+        {
+            count++;
+        }
+    }
+    if (count == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
 
-
-int main(){
+int main()
+{
 
     printf("jinruminganciguanlixitong\n");
-    
-    while(1){
-       printf("1--tianjiayigeminganci\n");
-       printf("2--shanchuyiduanminganci\n");
-       printf("3--xianshisuoyouminganci\n");
-       printf("4--tianhuanminganci\n");
-       printf("5--tuichu\n");
-       
-       printf("qingshuruxiangyingdegongnengbianhao\n");
-       int code;
-       scanf("%d",&code);
 
-       if(code == 1){
-           printf("qingshuruyaotianjiademinganzifu\n");
-           char m;
-           scanf("%c",&m);
-           scanf("%c",&m);
-           arr[index] = m;
-           index++;
-           printf("tianjiachenggong,dianjihuyichejixu\n");
-           char x;
-           scanf("%c",&x);
-           scanf("%c",&x);
+    while (1)
+    {
+        printf("1--tianjiayigeminganci\n");
+        printf("2--shanchuyiduanminganci\n");
+        printf("3--xianshisuoyouminganci\n");
+        printf("4--tianhuanminganci\n");
+        printf("5--tuichu\n");
 
+        printf("qingshuruxiangyingdegongnengbianhao\n");
+        int code;
+        scanf("%d", &code);
 
-
-       }
-        if(code == 2){
+        if (code == 1)
+        {
+            printf("qingshuruyaotianjiademinganzifu\n");
+            char m;
+            scanf("%c", &m);
+            scanf("%c", &m);
+            arr[index] = m;
+            index++;
+            printf("tianjiachenggong,dianjihuyichejixu\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
+        }
+        if (code == 2)
+        {
             printf("shancuzuihouyigeminganzifu\n");
             index--;
             printf("shanchuchenggong,dianjihuichejixu\n");
-           char x;
-           scanf("%c",&x);
-           scanf("%c",&x);
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
-        if(code == 3){
-           printf("xianshisuoyouyijingtianjiademinganzifu\n");
-            for (int i = 0; i < index; i++){
+        if (code == 3)
+        {
+            printf("xianshisuoyouyijingtianjiademinganzifu\n");
+            for (int i = 0; i < index; i++)
+            {
                 scanf("%c\n", arr[i]);
             }
             printf("xianshichenggong,dianjihuichejixu\n");
             char x;
             scanf("%c", &x);
             scanf("%c", &x);
-       }
-        if(code == 4){
-           
-       }
-        if(code == 5){
-           printf("tuichuchenggong\n");
-           break;
-       }
+        }
+        if (code == 4)
+        {
+            printf("qingshuruyiduanyingwen\n");
+            char neirong[1000];
+            scanf("%s", neirong);
+            for (int i = 0; 1; i++)
+            {
+                if (neirong[i] == '\0')
+                {
+                    break;
+                }
+                if (isMingan(arr[i]) == 1)
+                {
+                    neirong[i] = '*';
+                }
+                printf("%c", neirong[i]);
+            }
+            printf("\n");
 
+            printf("tihuanwancheng,dianjihuichejixu\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
 
+            if (code == 5)
+            {
+                printf("tuichuchenggong\n");
+                break;
+            }
+        }
     }
-
     return 0;
-
 }
